6bca3c2b8c9e76914119cfcc37ba4dd2a311d322
Anaïs <c7anais@gmail.com>
2021-11-02 22:03:12 +0100

    add turn over effect on cards and a launch game button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 // GLOBAL VARIABLES
 
-let cards = document.getElementsByClassName("hidden-face");
-let firstCard = "";
-let secondCard = "";
+let launchGame = document.getElementById("launchGame");
+let game = document.getElementById("game");
+let cards = document.getElementsByClassName("cardStillHidden");
+let firstCardFace = "";
+let secondCardFace = "";
 
 
 // FUNCTIONS
@@ -10,50 +12,65 @@ let secondCard = "";
 function chooseFirstCard() {
     for (let card of cards) {
         card.onclick = function(){
-        this.classList.remove("hidden-face");
-        let hiddenFirstCard = this;
-        firstCard = this.nextElementSibling.name;
-        chooseSecondCard(firstCard, hiddenFirstCard);
+        let firstCard = this;
+        firstCard.parentElement.classList.add("flip")
+        firstCard.classList.remove("cardStillHidden");
+        firstCardFace = firstCard.nextElementSibling.name;
+        chooseSecondCard(firstCardFace, firstCard);
         }
     }
 }
 
-function chooseSecondCard(firstCard, hiddenFirstCard) {
+function chooseSecondCard(firstCardFace, firstCard) {
     for (let card of cards) {
         card.onclick = function(){
-        this.classList.remove("hidden-face");
-        let hiddenSecondCard = this;
-        secondCard = this.nextElementSibling.name;
+        let secondCard = this;
+        secondCard.parentElement.classList.add("flip")
+        secondCard.classList.remove("cardStillHidden");
+        secondCardFace = secondCard.nextElementSibling.name;
         document.body.classList.add("disabledClick");
-        setTimeout(checkCard, 500, firstCard, hiddenFirstCard, secondCard, hiddenSecondCard);
+        setTimeout(checkCard, 700, firstCardFace, firstCard, secondCardFace, secondCard);
         }
     }
 }
 
-function checkCard(firstCard, hiddenFirstCard, secondCard, hiddenSecondCard) {
-    if (firstCard !== secondCard) {
-        console.log(firstCard + "false" + secondCard);
-        hiddenFirstCard.classList.add("hidden-face");
-        hiddenSecondCard.classList.add("hidden-face");
-        firstCard = "";
-        secondCard = ""; 
+function checkCard(firstCardFace, firstCard, secondCardFace, secondCard) {
+    if (firstCardFace !== secondCardFace) {
+        firstCard.parentElement.classList.remove("flip");
+        secondCard.parentElement.classList.remove("flip")
+        firstCard.classList.add("cardStillHidden");
+        secondCard.classList.add("cardStillHidden");
+        firstCardFace = "";
+        secondCardFace = ""; 
     } else {
-        console.log(firstCard + "true" + secondCard)
-        firstCard = "";
-        secondCard = "";
+        firstCardFace = "";
+        secondCardFace = "";
     }
     endGame();
 }
 
 function endGame() {
-     if (document.querySelector(".hidden-face")) {
+     if (document.querySelector(".cardStillHidden")) {
         document.body.classList.remove("disabledClick");
         chooseFirstCard(); 
     } else {
        alert("you win");
+        launchGame.classList.remove("d-none");
+        game.classList.add("d-none");
+        let backCards = document.querySelectorAll(".back");
+        backCards.forEach(function(backCard){
+            backCard.classList.add("cardStillHidden");
+            backCard.parentElement.classList.remove("flip")
+        });
+        document.body.classList.remove("disabledClick");
     }
 }
 
 // GAME
+
+launchGame.onclick = function() {
+    launchGame.classList.add("d-none");
+    game.classList.remove("d-none");
+}
 
 chooseFirstCard();
