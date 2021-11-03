8e7bc56174353b9d9dbe8f42303800aa8b0d52c8
Anaïs <c7anais@gmail.com>
2021-11-03 09:24:40 +0100

    deal cards randomly every new game

--- js/main.js
+++ js/main.js
@@ -68,41 +68,28 @@ let colors = [
     }
 ];
 
-function putColors() {
-    let images = document.querySelectorAll("img");
-    let i = Math.floor(Math.random() * colors.length);
-    images.forEach(function(image) {
-        console.log(i);
-        let color = colors[i]
-        console.log(color)
-        image.name = color["name"];
-        image.src = color["src"];
-        image.alt = color["alt"];
-        colors.splice(i, 1);
-        i = Math.floor(Math.random() * colors.length);
-        console.log(colors)
-    })
-    chooseFirstCard()
-}
-
 
 
 // FUNCTIONS
 
-function putColors() {
+function mixCards(colors) {
+    for(let i =colors.length-1 ; i>0 ;i--){
+        let j = Math.floor( Math.random() * (i + 1) );
+        [colors[i],colors[j]]=[colors[j],colors[i]];
+    }
+    dealCards();
+}
+
+function dealCards() {
     let images = document.querySelectorAll("img");
-    let i = Math.floor(Math.random() * colors.length);
+    let i = 0;
     images.forEach(function(image) {
-        console.log(i);
         let color = colors[i]
-        console.log(color)
         image.name = color["name"];
         image.src = color["src"];
         image.alt = color["alt"];
-        colors.splice(i, 1);
-        i = Math.floor(Math.random() * colors.length);
-        console.log(colors)
-    })
+        i++
+    });
     chooseFirstCard()
 }
 
@@ -168,6 +155,6 @@ function endGame() {
 launchGame.onclick = function() {
     launchGame.classList.add("d-none");
     game.classList.remove("d-none");
+    mixCards(colors);
 }
 
-putColors();
