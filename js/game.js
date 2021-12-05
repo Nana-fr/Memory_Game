cf3ef6a8d9266ec6699f5673a92793c09b60864f
Anaïs <c7anais@gmail.com>
2021-12-05 19:56:13 +0100

    responsive landscape/portrait

---
+++ js/game.js
@@ -0,0 +1,188 @@
+let p = document.getElementById("message");
+let nameRegex= /^[\p{Letter}]{3,20}$|^[\p{Letter}]+[-][\p{Letter}]+$|^[\p{Script=Han}]{2,3}$/gui;
+
+// setting variables
+let infoTheme = sessionStorage.getItem("infoTheme");
+let infoLevel = sessionStorage.getItem("infoLevel");
+let url = sessionStorage.getItem("url");
+
+// dashboard variables
+let dashboard = document.getElementById("dashboard");
+let themeInfo = document.getElementById("themeInfo");
+let levelInfo = document.getElementById("levelInfo");
+let nbMoves = document.getElementById("nbMoves");
+let maxMoves = document.getElementById("maxMoves");
+
+// cards variables
+let cards = document.getElementsByClassName("cardStillHidden");
+let firstCardFace = "";
+let secondCardFace = "";
+let deck = [];
+
+// FUNCTIONS
+
+function displayLayer(text) {
+    document.getElementById("layer").classList.remove("d-none");
+    p.innerHTML = text;
+}
+
+function closeLayer() {
+    p.classList.remove("overflow");
+    document.getElementById("layer").classList.add("d-none");
+}
+
+function setLevel(infoTheme, infoLevel, url) {
+    nbMoves.innerText = 0;
+    themeInfo.innerText= infoTheme;
+    if (infoLevel === "Difficult") {
+        maxMoves.innerText = "/9";
+    } else if (infoLevel === "Medium") {
+        maxMoves.innerText = "/12";
+    }
+    levelInfo.innerText = `Level: ${infoLevel}`;
+    displayLayer("<h3>Start!!!</h3>");
+    document.querySelector("audio").play();
+    getCards(infoTheme, infoLevel, url);
+}
+
+function getCards(infoTheme, infoLevel, url) {
+    fetch(url)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(response){
+                let tempArr = [];
+                for (let pairs in response) {
+                    tempArr.push(pairs)
+                } 
+                selectPairs(tempArr);
+                tempArr.splice(6,tempArr.length-6);
+                for (let tempArrPairs of tempArr) {
+                    for (let cards in response[tempArrPairs]) {
+                    deck.push(response[tempArrPairs][cards]);       
+                    }    
+                }
+            shuffleCards(deck, infoTheme, infoLevel);
+            })
+        }
+    });
+}
+
+function selectPairs(tempArr) {
+    for(let i =tempArr.length-1 ; i>0 ;i--){
+        let j = Math.floor( Math.random() * (i + 1) );
+        [tempArr[i],tempArr[j]]=[tempArr[j],tempArr[i]];
+    }
+}
+
+function shuffleCards(deck, infoTheme, infoLevel) {
+    for(let i =deck.length-1 ; i>0 ;i--){
+        let j = Math.floor( Math.random() * (i + 1) );
+        [deck[i],deck[j]]=[deck[j],deck[i]];
+    }
+    dealCards(infoTheme, infoLevel);
+}
+
+function dealCards(infoTheme, infoLevel) {
+    let images = document.querySelectorAll("img");
+    let i = 0;
+    images.forEach(function(image) {
+        let card = deck[i]
+        image.name = card["name"];
+        image.src = card["src"];
+        image.alt = card["alt"];
+        i++
+    });
+    chooseFirstCard(infoTheme, infoLevel)
+}
+
+// playing functions
+
+function chooseFirstCard(infoTheme, infoLevel) {
+    for (let card of cards) {
+        card.onclick = function(){
+        let firstCard = this;
+        firstCard.parentElement.classList.add("flip")
+        firstCard.classList.remove("cardStillHidden");
+        firstCardFace = firstCard.nextElementSibling.name;
+        chooseSecondCard(infoTheme, infoLevel, firstCardFace, firstCard);
+        }
+    }
+}
+
+function chooseSecondCard(infoTheme, infoLevel, firstCardFace, firstCard) {
+    for (let card of cards) {
+        card.onclick = function(){
+        let secondCard = this;
+        secondCard.parentElement.classList.add("flip")
+        secondCard.classList.remove("cardStillHidden");
+        secondCardFace = secondCard.nextElementSibling.name;
+        document.body.classList.add("disabledClick");
+        setTimeout(checkCard, 700, infoTheme, infoLevel, firstCardFace, firstCard, secondCardFace, secondCard);
+        }
+    }
+}
+
+function checkCard(infoTheme, infoLevel, firstCardFace, firstCard, secondCardFace, secondCard) {
+    if (firstCardFace !== secondCardFace) {
+        firstCard.parentElement.classList.remove("flip");
+        secondCard.parentElement.classList.remove("flip")
+        firstCard.classList.add("cardStillHidden");
+        secondCard.classList.add("cardStillHidden")
+    };
+    nbMoves.innerText++;
+    endGame(infoTheme, infoLevel);
+}
+
+function endGame(infoTheme, infoLevel) {
+    if (infoLevel === "Easy") {
+        if (document.querySelector(".cardStillHidden")) {
+            document.body.classList.remove("disabledClick");
+            chooseFirstCard(infoTheme, infoLevel); 
+        } else {
+            displayLayer(`<h3>Congratulations!!!</h3><br><h5>You win in ${nbMoves.innerText} moves.<h5>`);
+            checkRecords(infoTheme, infoLevel);
+        }
+    } else if (infoLevel === "Medium") {
+        if (!document.querySelector(".cardStillHidden") && nbMoves.innerText < 13) {
+            displayLayer(`<h3>Congratulations!!!</h3><br><h5>You win in ${nbMoves.innerText} moves.</h5>`);
+            checkRecords(infoTheme, infoLevel)
+        } else if (document.querySelector(".cardStillHidden") && nbMoves.innerText < 12) {
+            document.body.classList.remove("disabledClick");
+            chooseFirstCard(infoTheme, infoLevel);
+        } else {
+            displayLayer(`<h3>You lost!</h3><br><h5>${document.querySelectorAll(".cardStillHidden").length/2} pairs haven't been found.</h5>`);
+            resetGame();
+        }
+    } else {
+        if (!document.querySelector(".cardStillHidden") && nbMoves.innerText < 10) {
+            displayLayer(`<h3>Congratulations!!!</h3><br><h5>You win in ${nbMoves.innerText} moves.</h5>`);
+            checkRecords(infoTheme, infoLevel)
+        } else if (document.querySelector(".cardStillHidden") && nbMoves.innerText < 9) {
+            document.body.classList.remove("disabledClick");
+            chooseFirstCard(infoTheme, infoLevel);
+        } else {
+            displayLayer(`<h3>You lost!</h3><br><h5>${document.querySelectorAll(".cardStillHidden").length/2} pairs haven't been found.</h5>`);
+            resetGame();
+        }
+    }
+}
+
+function checkRecords(infoTheme, infoLevel) {
+    if (localStorage.getItem(`${infoTheme} ${infoLevel}`) === "," || localStorage.getItem(`${infoTheme} ${infoLevel}`).split(",")[1] > parseInt(nbMoves.innerText)) {
+        let name = prompt(`Congratulations, you set a new record\nPlease enter your name:`);
+        while (name.match(nameRegex) === null || name === null) {
+            name = prompt(`Your name must be between 3 and 20 letters long or 2 and 3 hanzi long:`);
+        }
+        localStorage.setItem(`${infoTheme} ${infoLevel}`, [name, nbMoves.innerText]);
+    }
+    resetGame();
+}
+
+// quit game function
+function resetGame() {
+    window.location.href="index.html"
+}
+
+// GAME
+
+setLevel(infoTheme, infoLevel, url)
--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@
 
 let startContainer = document.getElementById("startContainer");
 let launchGame = document.getElementById("launchGame");
-let rules = document.getElementById("messageInfo");
 let records = document.getElementById("records");
-let nameRegex= /^[\p{Letter}]{3,20}$|^[\p{Letter}]+[-][\p{Letter}]+$|^[\p{Script=Han}]{2,3}$/gui;
+
 
 // layer
 let p = document.getElementById("message");
@@ -15,30 +14,12 @@ let Colors = document.getElementById("Colors");
 let subtheme = document.getElementsByClassName("subtheme");
 let level = document.getElementById("level");
 
-// playing zone variables
-let game = document.getElementById("game");
-let board = document.getElementsByClassName("board");
-
-// dashboard variables
-let dashboard = document.getElementsByClassName("dashboard");
-let themeInfo = document.getElementsByClassName("themeInfo");
-let levelInfo = document.getElementsByClassName("levelInfo");
-let nbMoves = document.getElementsByClassName("nbMoves");
-let maxMoves = document.getElementsByClassName("maxMoves");
-
-// cards variables
-let cards = document.getElementsByClassName("cardStillHidden");
-let firstCardFace = "";
-let secondCardFace = "";
-let deck = [];
-
 
 // FUNCTIONS
 
 // layer functions
 function displayRules() {
     document.getElementById("layer").classList.remove("d-none");
-    p.classList.add("overflow");
     let httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
@@ -57,7 +38,6 @@ function displayLayer(text) {
 }
 
 function closeLayer() {
-    p.classList.remove("overflow");
     document.getElementById("layer").classList.add("d-none");
 }
 
@@ -208,193 +188,16 @@ function selectLevel(infoTheme, url) {
     for (let button of level.children) {
         button.onclick = function(){
             let clicked_id = this.id;
-            setLevel(infoTheme, url, clicked_id);
+            setSessionStorage(infoTheme, url, clicked_id);
         }
     }
 }
 
-function setLevel(infoTheme, url, clicked_id) {
-    level.classList.add("d-none");
-    game.classList.remove("d-none");
-    let size;
-    if (window.innerWidth<992){
-        size = 0;
-        dashboard[0].previousElementSibling.classList.add("d-none");
-    } else {
-        board[0].style.width="62vw";
-        size = 1;
-    }
-    dashboard[size].classList.remove("d-none");
-    nbMoves[size].innerText = 0;
-    themeInfo[size].innerText= infoTheme;
-    let infoLevel = clicked_id;
-    if (infoLevel === "Difficult") {
-        maxMoves[size].innerText = "/9";
-    } else if (infoLevel === "Medium") {
-        maxMoves[size].innerText = "/12";
-    }
-    levelInfo[size].innerText = `Level: ${infoLevel}`;
-    displayLayer("<h3>Start!!!</h3>");
-    document.querySelector("audio").play();
-    getCards(infoTheme, infoLevel, url, size);
+function setSessionStorage(infoTheme, url, clicked_id) {
+    sessionStorage.setItem('infoTheme', infoTheme);
+    sessionStorage.setItem('infoLevel', clicked_id);
+    sessionStorage.setItem('url', url);
 }
-
-function getCards(infoTheme, infoLevel, url, size) {
-    fetch(url)
-    .then(function(response) {
-        if(response.ok) {
-            response.json().then(function(response){
-                let tempArr = [];
-                for (let pairs in response) {
-                    tempArr.push(pairs)
-                } 
-                selectPairs(tempArr);
-                tempArr.splice(6,tempArr.length-6);
-                for (let tempArrPairs of tempArr) {
-                    for (let cards in response[tempArrPairs]) {
-                    deck.push(response[tempArrPairs][cards]);       
-                    }    
-                }
-            shuffleCards(deck, infoTheme, infoLevel, size);
-            })
-        }
-    });
-}
-
-function selectPairs(tempArr) {
-    for(let i =tempArr.length-1 ; i>0 ;i--){
-        let j = Math.floor( Math.random() * (i + 1) );
-        [tempArr[i],tempArr[j]]=[tempArr[j],tempArr[i]];
-    }
-}
-
-function shuffleCards(deck, infoTheme, infoLevel, size) {
-    for(let i =deck.length-1 ; i>0 ;i--){
-        let j = Math.floor( Math.random() * (i + 1) );
-        [deck[i],deck[j]]=[deck[j],deck[i]];
-    }
-    dealCards(infoTheme, infoLevel, size);
-}
-
-function dealCards(infoTheme, infoLevel, size) {
-    let images = document.querySelectorAll("img");
-    let i = 0;
-    images.forEach(function(image) {
-        let card = deck[i]
-        image.name = card["name"];
-        image.src = card["src"];
-        image.alt = card["alt"];
-        i++
-    });
-    chooseFirstCard(infoTheme, infoLevel, size)
-}
-
-// playing functions
-
-function chooseFirstCard(infoTheme, infoLevel, size) {
-    for (let card of cards) {
-        card.onclick = function(){
-        let firstCard = this;
-        firstCard.parentElement.classList.add("flip")
-        firstCard.classList.remove("cardStillHidden");
-        firstCardFace = firstCard.nextElementSibling.name;
-        chooseSecondCard(infoTheme, infoLevel, size, firstCardFace, firstCard);
-        }
-    }
-}
-
-function chooseSecondCard(infoTheme, infoLevel, size, firstCardFace, firstCard) {
-    for (let card of cards) {
-        card.onclick = function(){
-        let secondCard = this;
-        secondCard.parentElement.classList.add("flip")
-        secondCard.classList.remove("cardStillHidden");
-        secondCardFace = secondCard.nextElementSibling.name;
-        document.body.classList.add("disabledClick");
-        setTimeout(checkCard, 700, infoTheme, infoLevel, size, firstCardFace, firstCard, secondCardFace, secondCard);
-        }
-    }
-}
-
-function checkCard(infoTheme, infoLevel, size, firstCardFace, firstCard, secondCardFace, secondCard) {
-    if (firstCardFace !== secondCardFace) {
-        firstCard.parentElement.classList.remove("flip");
-        secondCard.parentElement.classList.remove("flip")
-        firstCard.classList.add("cardStillHidden");
-        secondCard.classList.add("cardStillHidden")
-    };
-    nbMoves[size].innerText++;
-    endGame(infoTheme, infoLevel, size);
-}
-
-function endGame(infoTheme, infoLevel, size) {
-    if (infoLevel === "Easy") {
-        if (document.querySelector(".cardStillHidden")) {
-            document.body.classList.remove("disabledClick");
-            chooseFirstCard(infoTheme, infoLevel, size); 
-        } else {
-            displayLayer(`<h3>Congratulations!!!</h3><br><h5>You win in ${nbMoves[size].innerText} moves.<h5>`);
-            checkRecords(infoTheme, infoLevel, size);
-        }
-    } else if (infoLevel === "Medium") {
-        if (!document.querySelector(".cardStillHidden") && nbMoves[size].innerText < 13) {
-            displayLayer(`<h3>Congratulations!!!</h3><br><h5>You win in ${nbMoves[size].innerText} moves.</h5>`);
-            checkRecords(infoTheme, infoLevel, size)
-        } else if (document.querySelector(".cardStillHidden") && nbMoves[size].innerText < 12) {
-            document.body.classList.remove("disabledClick");
-            chooseFirstCard(infoTheme, infoLevel, size);
-        } else {
-            displayLayer(`<h3>You lost!</h3><br><h5>${document.querySelectorAll(".cardStillHidden").length/2} pairs haven't been found.</h5>`);
-            resetGame();
-        }
-    } else {
-        if (!document.querySelector(".cardStillHidden") && nbMoves[size].innerText < 10) {
-            displayLayer(`<h3>Congratulations!!!</h3><br><h5>You win in ${nbMoves[size].innerText} moves.</h5>`);
-            checkRecords(infoTheme, infoLevel, size)
-        } else if (document.querySelector(".cardStillHidden") && nbMoves[size].innerText < 9) {
-            document.body.classList.remove("disabledClick");
-            chooseFirstCard(infoTheme, infoLevel, size);
-        } else {
-            displayLayer(`<h3>You lost!</h3><br><h5>${document.querySelectorAll(".cardStillHidden").length/2} pairs haven't been found.</h5>`);
-            resetGame();
-        }
-    }
-}
-
-function checkRecords(infoTheme, infoLevel, size) {
-    if (localStorage.getItem(`${infoTheme} ${infoLevel}`) === "," || localStorage.getItem(`${infoTheme} ${infoLevel}`).split(",")[1] > parseInt(nbMoves[size].innerText)) {
-        let name = prompt(`Congratulations, you set a new record\nPlease enter your name:`);
-        while (name.match(nameRegex) === null || name === null) {
-            name = prompt(`Your name must be between 3 and 20 letters long or 2 and 3 hanzi long:`);
-        }
-        localStorage.setItem(`${infoTheme} ${infoLevel}`, [name, nbMoves[size].innerText]);
-    }
-    resetGame();
-}
-
-// quit game function
-function resetGame() {
-    let size;
-    if (window.innerWidth<992){
-        size = 0;
-        dashboard[0].previousElementSibling.classList.remove("d-none");
-    } else {
-        board[0].style.width="100vw";
-        size = 1;
-    }
-    let backCards = document.querySelectorAll(".back");
-    backCards.forEach(function(backCard){
-        backCard.classList.add("cardStillHidden");
-        backCard.parentElement.classList.remove("flip")
-    });
-    dashboard[size].classList.add("d-none");
-    maxMoves[size].innerText = "";
-    deck = [];
-    document.body.classList.remove("disabledClick");
-    game.classList.add("d-none");
-    startContainer.classList.remove("d-none");
-}
-
 
 // LOCAL STORAGE
 
@@ -402,7 +205,7 @@ if(!localStorage.getItem("informed")){
     setStorage();
 }
 
-// GAME
+// LAUNCH GAME
 
 launchGame.onclick = function() {
     startContainer.classList.add("d-none");
